21f0f3d48877022e710e7eb3ec326beec6c965ce
Alejandro Ramirez <alejandro.ramirez.v@ucb.edu.bo>
2025-05-28 18:58:26 -0400

    Creacion de componentes

---
+++ src/app/componentes/usuario.componente/usuario.componente.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario.componente',
+  imports: [],
+  templateUrl: './usuario.componente.html',
+  styleUrl: './usuario.componente.css'
+})
+export class UsuarioComponente {
+
+}
